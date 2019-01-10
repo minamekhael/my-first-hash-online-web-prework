5e4be2ee74330ab7bde27c45907d09db4335db85
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-10 23:21:21 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -15,8 +15,6 @@ def retrieval
                  }
                  
   shipping_manifest["oil paintings"]
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
 end
 
 def adding
@@ -25,14 +23,9 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-  # add 2 muskets to the shipping_manifest hash below
-
-
-
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
-
+                 
+    shipping_manifest{"muskets"} => 2
+    shipping_manifest{"gun powder"} => 4
+    
+    return shipping_manifest
 end
